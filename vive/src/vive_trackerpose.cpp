87d197c830bb4d21591f67cb1e3b9d20c55b08f0
LeoKang0801 <bartkang0818@gmail.com>
2023-05-01 19:34:35 +0000

    FIX: ekf match bug

--- vive/src/vive_trackerpose.cpp
+++ vive/src/vive_trackerpose.cpp
@@ -57,7 +57,7 @@ private:
     double tole_with_ekf_angle;
     bool ekf_active;
     bool in_boundry_;
-    int pub_count;
+    int ekf_count;
 };
 Robot::Robot(ros::NodeHandle nh_g, ros::NodeHandle nh_l) {
     nh = nh_g;
@@ -73,7 +73,7 @@ Robot::Robot(ros::NodeHandle nh_g, ros::NodeHandle nh_l) {
     ok &= nh_.getParam("tole_with_ekf", tole_with_ekf);
     ok &= nh_.getParam("tole_with_ekf_angle", tole_with_ekf_angle);
     ok &= nh_.getParam("ekf_active", ekf_active);
-    ok &= nh_.getParam("pub_count",pub_count);
+    ok &= nh_.getParam("ekf_count",ekf_count);
     ok &= nh_.getParam("rot_from_tracker_W", rot_from_tracker.W);
     ok &= nh_.getParam("rot_from_tracker_X", rot_from_tracker.X);
     ok &= nh_.getParam("rot_from_tracker_Y", rot_from_tracker.Y);
@@ -154,9 +154,6 @@ void Robot::publish_vive_raw_pose() {
     }
 }
 void Robot::publish_vive_pose() {
-    if(has_tf && in_boundry_ && is_stable && pub_count > 0){
-        pub_count--;
-    }
     if (has_tf && match_ekf && in_boundry_ && is_stable) {
         pose.pose.pose.orientation.w = pose_filter.W;
         pose.pose.pose.orientation.x = pose_filter.X;
@@ -197,7 +194,8 @@ void Robot::print_pose(int unit_) {
 }
 void Robot::ekf_sub_callback(const geometry_msgs::PoseWithCovarianceStamped& msg_) {
     match_ekf = true;
-    if (ekf_active && has_tf && pub_count == 0) {
+    if (ekf_active && has_tf && ekf_count > 0) ekf_count --;
+    if (ekf_active && has_tf && ekf_count == 0) {
         VIVEPOSE ekf_p;
         ekf_p.x = msg_.pose.pose.position.x;
         ekf_p.y = msg_.pose.pose.position.y;
@@ -210,8 +208,12 @@ void Robot::ekf_sub_callback(const geometry_msgs::PoseWithCovarianceStamped& msg
         double d = (double)sqrt(pow(ekf_p.x - pose_filter.x, 2) + pow(ekf_p.y - pose_filter.y, 2));
         if (d > tole_with_ekf) match_ekf = false;
 
-        pose_filter.yaw = (double)2 * acos(pose_filter.W) * 180 / 3.14159;
-        ekf_p.yaw = (double)2 * acos(ekf_p.W) * 180 / 3.14159;
+        tf::Quaternion q_ekf(ekf_p.X, ekf_p.Y, ekf_p.Z, ekf_p.W);
+        tf::Quaternion q_filter(pose_filter.X, pose_filter.Y, pose_filter.Z, pose_filter.W);
+        ekf_p.yaw = (double) q_ekf.getAngle() * 180 / 3.14159;
+        pose_filter.yaw = (double) q_filter.getAngle() * 180 / 3.14159;
+        if(ekf_p.Z * pose_filter.Z < 0) pose_filter.yaw = 360 - pose_filter.yaw;
+
         if (pow((pose_filter.yaw - ekf_p.yaw), 2) > pow(tole_with_ekf_angle, 2)) match_ekf = false;
 
         // ROS_INFO("yaw(vive/ekf): %f/%f", pose_filter.yaw, ekf_p.yaw);
