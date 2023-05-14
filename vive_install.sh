455b8d13198f6f5ee3d576562b44311a4f173ea5
xangsx <liupinghua.angus@gmail.com>
2023-05-14 18:32:46 +0000

    MODIFY : vive_install.sh

--- vive_install.sh
+++ vive_install.sh
@@ -8,8 +8,10 @@ fi
 
 PACKAGES_PATH="$( find ~ -name vive | awk '{print $1}' | head -1)"
 
+
 cd $PACKAGES_PATH
 cd $PACKAGES_PATH/libsurvive
 sudo cp ./useful_files/81-vive.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
+sudo apt-get install build-essential zlib1g-dev libx11-dev libusb-1.0-0-dev freeglut3-dev liblapacke-dev libopenblas-dev libatlas-base-dev cmake
 make
